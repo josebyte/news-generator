dc4f3a697a77bdad70f264b4bb637f6115e00bbe
Joseba <mjoseba@gmail.com>
2026-02-14 16:01:12 +0100

    image test

--- src/index.mjs
+++ src/index.mjs
@@ -14,17 +14,25 @@ export async function processArticle(noticia, config) {
     if (art.image) {
         const imagesFolder = path.join(process.cwd(), 'public/images/noticias');
 
+        console.log("art.image")
+        console.log(art.image)
         let finalImageUrl = art.image || null;
 
         // Si la IA devuelve una ruta relativa, intentamos reconstruirla
         if (finalImageUrl && !finalImageUrl.startsWith('http')) {
             const domain = new URL(noticia.sourceUrl).origin;
+            console.log("domain")
+            console.log(domain)
             finalImageUrl = new URL(finalImageUrl, domain).href;
         }
+        console.log("finalImageUrl")
+        console.log(finalImageUrl)
 
         const localPath = await descargarImagen(finalImageUrl, imagesFolder);
         art.image = localPath || art.image;
     }
+
+    console.log(fileName)
 
     // 3. Guardar Markdown
     const fileName = await guardarNoticia(art, config, noticia.sourceUrl);
